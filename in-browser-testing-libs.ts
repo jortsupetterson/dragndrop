4476858146c19c7fe4a3b504355e8af2782c6bba
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-29 19:06:36 +0300

    chore: drag and watch

--- in-browser-testing-libs.ts
+++ in-browser-testing-libs.ts
@@ -19,19 +19,32 @@ background: red;
 }
 
 void dragTarget.addEventListener('pointerdown', async (event) => {
-  void drag(event, async (dragged, watcher) => {
-    const append = () => {
-      watcher.appendChild(dragged)
+  dragTarget.style.transition = `transform 0s ease`
+  let append: (() => void) | undefined
+  void drag(
+    event,
+    async (dragged, watcher) => {
+      if (append) dragged.removeEventListener('pointerup', append)
+      append = () => {
+        watcher.appendChild(dragged)
+        if (append) dragged.removeEventListener('pointerup', append)
+        append = undefined
+      }
+      dragged.addEventListener('pointerup', append)
+    },
+    async (dragged) => {
+      if (!append) return
       dragged.removeEventListener('pointerup', append)
+      append = undefined
     }
-    dragged.addEventListener('pointerup', append)
-  })
+  )
   for (const watcher of watchers) {
     void startWatch(watcher, dragTarget)
   }
 })
 
 void dragTarget.addEventListener('pointerup', async () => {
+  dragTarget.style.transition = `transform 0.5s ease`
   dragTarget.style.transform = `translate(0px, 0px)`
   delete dragTarget.dataset.x
   delete dragTarget.dataset.y
