521fd4581d2ffc4a3b591d3bb960fb35a43d70cf
Jori Lethinen <lehtinenjori03@gmail.com>
2026-04-30 10:34:20 +0300

    chore: to classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { drag } from './drag/index.js'
 export { DragArea } from './DragArea/index.js'
-export { DragTarget } from './dragTarget/index.js'
+export { DragTarget } from './DragTarget/index.js'
 export { startWatch, stopWatch } from './watch/index.js'
